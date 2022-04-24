505cc312204a3163083e2e52f28cd2299a851638
Aranzasuu <>
2022-04-24 00:09:34 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -43,9 +43,33 @@ TreeMap *createTreeMap(int (*lower_than)(void *key1, void *key2)) {
 }
 
 void insertTreeMap(TreeMap *tree, void *key, void *value) {
-  //TreeNode *new = createTreeNode(key,value);
-	//TreeNode *aux = tree->current;
-  
+  TreeNode *new = createTreeNode(key,value);
+	TreeNode *aux = tree->current;
+  if(tree->root == NULL){
+    tree->root = new;
+    return;
+  }
+
+  while(aux != NULL){
+    if(tree->lower_than(key, aux->pair->key) == 1){
+      if(aux->left == NULL){
+        aux->left = new;
+        new->parent = aux;
+        aux = aux->left;
+        return;
+      }
+      aux = aux->left;
+    }
+    else{
+      if(aux->right == NULL){
+        aux->right = new;
+        new->parent = aux;
+        aux = aux->right;
+        return;
+      }
+      aux = aux->right;
+    }
+  }
 }
 
 TreeNode *minimum(TreeNode *x) {
